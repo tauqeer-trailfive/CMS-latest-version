7e69a26e7a9eae2b4925991d23a558833854e697
tauqeer-trailfive <108520807+tauqeer-trailfive@users.noreply.github.com>
2023-08-04 18:19:58 +0500

    NEW PURPOSED CMS For Djaminn

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import path from "path";
+import fs from "fs";
+import react from "@vitejs/plugin-react";
+import visualizer from "rollup-plugin-visualizer";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    visualizer({
+      open: process.env.NODE_ENV !== "CI",
+      filename: "./dist/stats.html",
+    }),
+  ],
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    port: 8000,
+    host: true,
+  },
+  base: "./",
+  esbuild: {
+    keepNames: true,
+  },
+  build: {
+    sourcemap: true,
+  },
+});
