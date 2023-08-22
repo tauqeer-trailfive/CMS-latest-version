2c7cf4fc0ff9f3852e5c985078760a5fc8a5d146
tauqeer-trailfive <108520807+tauqeer-trailfive@users.noreply.github.com>
2023-08-22 09:49:42 +0500

    Making editable user

--- src/dataProvider/graphql.ts
+++ src/dataProvider/graphql.ts
@@ -43,26 +43,6 @@ const customBuildQuery: BuildQueryFactory = (introspectionResults) => {
   return (type, resource, params) => {
     /* User */
     if (resource === "User" && type === "GET_LIST") {
-      //console.log("in GetList Users", params);
-      //console.log(params.filter.role);
-      let defaultORderBtCreation: string;
-      if (params.filter.role === undefined) {
-        params.filter.role = [
-          "SUPERADMIN",
-          "ADMIN",
-          "USER",
-          "ANON",
-          "CONTENTCREATOR",
-          "SERVICE",
-          "QA",
-        ];
-      }
-      if (params.sort.field === undefined && params.sort.order === undefined) {
-        defaultORderBtCreation = "createdAt_ASC";
-      } else {
-        defaultORderBtCreation = `${params.sort.field}_${params.sort.order}`;
-      }
-
       return {
         query: gql`
           query AllUsers(
@@ -114,7 +94,7 @@ const customBuildQuery: BuildQueryFactory = (introspectionResults) => {
             ...(params.filter.email && { email_contains: params.filter.email }),
             ...(params.filter.role && { role_in: params.filter.role }),
           },
-          orderBy: defaultORderBtCreation,
+          orderBy: `${params.sort.field}_${params.sort.order}`,
           take: params.pagination.perPage,
           skip: params.pagination.perPage * (params.pagination.page - 1),
         },
@@ -163,240 +143,182 @@ const customBuildQuery: BuildQueryFactory = (introspectionResults) => {
       };
     }
 
-    // if (resource === "User" && type === "UPDATE") {
-    //   delete params.data.id;
-    //   delete params.data.__typename;
-    //   delete params.data.createdAt;
-    //   if (
-    //     (params.previousData.isValidated === null ||
-    //       params.previousData.isValidated === false) &&
-    //     params.data.isValidated === false
-    //   ) {
-    //     params.data.isValidated = false;
-    //   } else {
-    //     // const decodedToken = decodeJwt(localStorage.getItem("token"));
-    //     // params.data.validatedBy = { connect: { id: decodedToken.userId } };
-    //     params.data.validatedBy = {
-    //       connect: { id: localStorage.getItem("user_id") },
-    //     };
-    //   }
+    if (resource === "User" && type === "UPDATE") {
+      delete params.data.id;
+      delete params.data.__typename;
+      delete params.data.createdAt;
+      if (
+        (params.previousData.isValidated === null ||
+          params.previousData.isValidated === false) &&
+        params.data.isValidated === false
+      ) {
+        params.data.isValidated = false;
+      } else {
+        // const decodedToken = decodeJwt(localStorage.getItem("token"));
+        // params.data.validatedBy = { connect: { id: decodedToken.userId } };
+        params.data.validatedBy = {
+          connect: { id: localStorage.getItem("user_id") },
+        };
+      }
 
-    //   if (
-    //     params.data.musicalInstruments
-    //     //  && params.data.tracks.length !== 0
-    //   ) {
-    //     params.data.musicalInstruments = updateInstrumentsinUser(
-    //       params.data.musicalInstruments,
-    //       params.previousData.musicalInstruments
-    //     );
-    //   }
-    //   return {
-    //     query: gql`
-    //       mutation updateUser(
-    //         $data: UserUpdateInput!
-    //         $where: UserWhereUniqueInput!
-    //       ) {
-    //         data: updateUser(data: $data, where: $where) {
-    //           id
-    //           email
-    //           name
-    //           artistName
-    //           role
-    //           avatarUrl
-    //           audioCorePluginAllowUser
-    //           headerImage
-    //           isDeveloper
-    //           isValidated
-    //           musicalInstruments {
-    //             id
-    //             name
-    //             rank
-    //           }
-    //         }
-    //       }
-    //     `,
+      // if (
+      //   params.data.musicalInstruments
+      //   //  && params.data.tracks.length !== 0
+      // ) {
+      //   params.data.musicalInstruments = updateInstrumentsinUser(
+      //     params.data.musicalInstruments,
+      //     params.previousData.musicalInstruments
+      //   );
+      // }
+      return {
+        query: gql`
+          mutation updateUser(
+            $data: UserUpdateInput!
+            $where: UserWhereUniqueInput!
+          ) {
+            data: updateUser(data: $data, where: $where) {
+              id
+              email
+              name
+              artistName
+              role
+              bio
+              avatarUrl
+              audioCorePluginAllowUser
+              headerImage
+              isDeveloper
+              isValidated
+              musicalInstruments {
+                id
+                name
+                rank
+              }
+            }
+          }
+        `,
 
-    //     variables: {
-    //       data: {
-    //         name: params.data.name,
-    //         email: params.data.email,
-    //         artistName: params.data.artistName,
-    //         role: params.data.role,
-    //         // avatarUrl: params.data.avatarUrl,
-    //         musicalInstruments: params.data.musicalInstruments,
-    //       },
-    //       where: { id: params.id },
-    //     },
-    //     options: { fetchPolicy: "network-only" },
-    //     // tslint:disable-next-line:object-literal-sort-keys
-    //     parseResponse: (response: any) => {
-    //       localStorage.removeItem("avatar_url");
-    //       return {
-    //         data: response.data.data,
-    //         total: response.data.data.length,
-    //       };
-    //     },
-    //   };
-    // }
+        variables: {
+          data: {
+            name: params.data.name,
+            email: params.data.email,
+            artistName: params.data.artistName,
+            role: params.data.role,
+            bio: params.data.bio,
+            // musicalInstruments: params.data.musicalInstruments,
+          },
+          where: { id: params.id },
+        },
+        options: { fetchPolicy: "network-only" },
+        parseResponse: (response: any) => {
+          localStorage.removeItem("avatar_url");
+          return {
+            data: response.data.data,
+            total: response.data.data.length,
+          };
+        },
+      };
+    }
 
-    // if (resource === "User" && type === "GET_MANY") {
-    //   return {
-    //     query: gql`
-    //       query AllUsers($filter: UserWhereInput, $orderBy: UserOrderByInput) {
-    //         data: allUsersV1(filter: $filter, orderBy: $orderBy) {
-    //           id
-    //           createdAt
-    //           email
-    //           name
-    //           artistName
-    //           role
-    //           avatarUrl
-    //           audioCorePluginAllowUser
-    //           headerImage
-    //           isValidated
-    //           isDeveloper
-    //           musicalInstruments {
-    //             id
-    //             name
-    //             rank
-    //           }
-    //         }
-    //         usersMeta(where: $filter) {
-    //           count
-    //         }
-    //       }
-    //     `,
-    //     variables: {
-    //       filter: { id_in: params.ids },
-    //       // data: params,
-    //       // orderBy: `${params.sort.order}`,
-    //       // where: {name_contains: params.filter.name},
-    //       // where:{
-    //       // name_contains: (params.filter.name || params.filter.q),
-    //       // // role_in: params.filter.role,
-    //       // },
-    //     },
-    //     options: { fetchPolicy: "network-only" },
-    //     // tslint:disable-next-line:object-literal-sort-keys
-    //     parseResponse: (response: any) => {
-    //       return {
-    //         data: response.data.data,
-    //         total: response.data.usersMeta.count,
-    //       };
-    //     },
-    //   };
-    // }
+    if (resource === "User" && type === "GET_MANY") {
+      return {
+        query: gql`
+          query AllUsers($filter: UserWhereInput, $orderBy: UserOrderByInput) {
+            data: allUsersV1(filter: $filter, orderBy: $orderBy) {
+              id
+              createdAt
+              email
+              name
+              artistName
+              role
+              avatarUrl
+              audioCorePluginAllowUser
+              headerImage
+              isValidated
+              isDeveloper
+              musicalInstruments {
+                id
+                name
+                rank
+              }
+            }
+            usersMeta(where: $filter) {
+              count
+            }
+          }
+        `,
+        variables: {
+          filter: { id_in: params.ids },
+          // data: params,
+          // orderBy: `${params.sort.order}`,
+          // where: {name_contains: params.filter.name},
+          // where:{
+          // name_contains: (params.filter.name || params.filter.q),
+          // // role_in: params.filter.role,
+          // },
+        },
+        options: { fetchPolicy: "network-only" },
+        // tslint:disable-next-line:object-literal-sort-keys
+        parseResponse: (response: any) => {
+          return {
+            data: response.data.data,
+            total: response.data.usersMeta.count,
+          };
+        },
+      };
+    }
 
-    // if (resource === "User" && type === "DELETE") {
-    //   return {
-    //     query: gql`
-    //       mutation deleteUser($where: UserWhereUniqueInput!) {
-    //         data: deleteUser(where: $where) {
-    //           id
-    //           createdAt
-    //           email
-    //           name
-    //           artistName
-    //           role
-    //           isValidated
-    //           avatarUrl
-    //           audioCorePluginAllowUser
-    //           headerImage
-    //         }
-    //       }
-    //     `,
-    //     variables: { where: { id: params.id } },
-    //     options: { fetchPolicy: "network-only" },
-    //     // tslint:disable-next-line:object-literal-sort-keys
-    //     parseResponse: (response: any) => {
-    //       return {
-    //         data: response.data.data,
-    //         // total: response.data.data.length,
-    //       };
-    //     },
-    //   };
-    // }
+    if (resource === "User" && type === "DELETE") {
+      return {
+        query: gql`
+          mutation deleteUser($where: UserWhereUniqueInput!) {
+            data: deleteUser(where: $where) {
+              id
+              createdAt
+              email
+              name
+              artistName
+              role
+              isValidated
+              avatarUrl
+              audioCorePluginAllowUser
+              headerImage
+            }
+          }
+        `,
+        variables: { where: { id: params.id } },
+        options: { fetchPolicy: "network-only" },
+        // tslint:disable-next-line:object-literal-sort-keys
+        parseResponse: (response: any) => {
+          return {
+            data: response.data.data,
+            // total: response.data.data.length,
+          };
+        },
+      };
+    }
 
-    // if (resource === "User" && type === "DELETE_MANY") {
-    //   return {
-    //     query: gql`
-    //       mutation deleteUsers($where: UserWhereInput!) {
-    //         data: deleteUsers(where: $where) {
-    //           count
-    //         }
-    //       }
-    //     `,
-    //     variables: {
-    //       where: { id_in: params.ids },
-    //     },
-    //     options: { fetchPolicy: "network-only" },
-    //     // tslint:disable-next-line:object-literal-sort-keys
-    //     parseResponse: (response: any) => {
-    //       return {
-    //         data: [response.data.data],
-    //         // total: response.data.data.length,
-    //       };
-    //     },
-    //   };
-    // }
-
-    // if (resource === "User" && type === "CREATE") {
-    //   let randomAvatar =
-    //     RandomAvatars[Math.floor(Math.random() * RandomAvatars.length)];
-    //   //console.log("randomAvatar", randomAvatar);
-    //   return {
-    //     query: gql`
-    //       mutation createUserByCMS(
-    //         $password: String!
-    //         $email: String!
-    //         $artistName: String!
-    //         $role: UserRoles!
-    //         $name: String!
-    //         $avatarUrl: String!
-    //       ) {
-    //         data: createUserByCMS(
-    //           password: $password
-    //           email: $email
-    //           artistName: $artistName
-    //           role: $role
-    //           name: $name
-    //           avatarUrl: $avatarUrl
-    //         ) {
-    //           user {
-    //             id
-    //             email
-    //             name
-    //             artistName
-    //             role
-    //             isValidated
-    //             avatarUrl
-    //             audioCorePluginAllowUser
-    //             headerImage
-    //           }
-    //         }
-    //       }
-    //     `,
-    //     // variables: params.data,
-    //     variables: {
-    //       password: params.data.password,
-    //       email: params.data.email,
-    //       artistName: params.data.artistName,
-    //       role: params.data.role,
-    //       name: params.data.name,
-    //       avatarUrl: randomAvatar,
-    //       // audioCorePluginAllowUser: params.data.audioCorePluginAllowUser,
-    //     },
-    //     options: { fetchPolicy: "network-only" },
-    //     // tslint:disable-next-line:object-literal-sort-keys
-    //     parseResponse: (response: any) => {
-    //       //console.log("Response From create", response);
-    //       return {
-    //         data: response.data.data.user,
-    //         // total: response.data.data.length,
-    //       };
-    //     },
-    //   };
-    // }
+    if (resource === "User" && type === "DELETE_MANY") {
+      return {
+        query: gql`
+          mutation deleteUsers($where: UserWhereInput!) {
+            data: deleteUsers(where: $where) {
+              count
+            }
+          }
+        `,
+        variables: {
+          where: { id_in: params.ids },
+        },
+        options: { fetchPolicy: "network-only" },
+        // tslint:disable-next-line:object-literal-sort-keys
+        parseResponse: (response: any) => {
+          return {
+            data: [response.data.data],
+            // total: response.data.data.length,
+          };
+        },
+      };
+    }
 
     if (resource === "User" && type === "CREATE") {
       const randomAvatar =
