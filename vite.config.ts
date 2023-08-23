64bc07a3aa5259cc3211738bbd9383231ad02200
tauqeer-trailfive <108520807+tauqeer-trailfive@users.noreply.github.com>
2023-08-23 18:31:58 +0500

    Change in Vite Config (Give PORT from env file)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import fs from "fs";
 import react from "@vitejs/plugin-react";
 import visualizer from "rollup-plugin-visualizer";
+const env = loadEnv("", "./");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,11 +18,11 @@ export default defineConfig({
     "process.env": process.env,
   },
   server: {
-    port: 3000,
+    port: parseInt(env.VITE_PORT),
     host: true,
   },
   preview: {
-    port: 3000,
+    port: parseInt(env.VITE_PORT),
     host: true,
   },
   base: "./",
