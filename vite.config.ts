54982bbbe51f9fcfbf3ed6f7eb05801ad3fa3923
tauqeer-trailfive <108520807+tauqeer-trailfive@users.noreply.github.com>
2023-09-05 10:33:51 +0500

    Changes in Vite Configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig, loadEnv } from "vite";
-import path from "path";
-import fs from "fs";
 import react from "@vitejs/plugin-react";
-import visualizer from "rollup-plugin-visualizer";
 const env = loadEnv("", "./");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: process.env.NODE_ENV !== "CI",
-      filename: "./dist/stats.html",
-    }),
-  ],
+  plugins: [react()],
   define: {
     "process.env": process.env,
   },
@@ -26,10 +17,14 @@ export default defineConfig({
     host: true,
   },
   base: "./",
-  esbuild: {
-    keepNames: true,
-  },
   build: {
-    sourcemap: true,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
   },
 });
